1cfba5097f9b81712dd30338af80b40f925c3d92
Kuzmicheva Ljudmila <l.kuzmicheva@cft.ru>
2020-12-28 09:54:12 +0700

    apigateway

---
+++ auth-service/src/main/java/ru/kl/auth/model/Headers.java
@@ -0,0 +1,8 @@
+package ru.kl.auth.model;
+
+public class Headers {
+
+    public static final String AUTH_ROLES_HEADER_NAME = "X-Auth-Roles";
+
+    public static final String AUTH_CLIENT_HEADER_NAME = "X-Auth-Client";
+}
